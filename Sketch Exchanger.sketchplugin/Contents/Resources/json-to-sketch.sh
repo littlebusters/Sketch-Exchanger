e57cfffb2541998410debb5c4d83f4b186c4d8f3
littlebusters <little.busters.reply@gmail.com>
2017-05-09 07:41:54 +0900

    init

---
+++ Sketch Exchanger.sketchplugin/Contents/Resources/json-to-sketch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+  echo "Specify the directory and then execute."
+  exit 1
+fi
+
+directoryName=$1
+
+if [ -d ${directoryName} ]; then
+  cd ${directoryName} && zip -o ${directoryName}.sketch -r *
+fi
---
+++ Sketch Exchanger.sketchplugin/Contents/Resources/sketch-to-json.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+  echo "Specify the sketch file and then execute."
+  exit 1
+fi
+
+fileName=$1
+fileData="${fileName}.sketch"
+
+if [ -f ${fileData} ]; then
+  unzip ${fileData} -d ${fileName}
+  find . -name '*.json' -exec sh -c 'cat {} | jq -r . > {}.1 && mv {}.1 {}' \;
+  rm -rf ${fileName}/__MACOSX ${fileName}/.DS_Store
+fi
