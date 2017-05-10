c87f867ad792b1616449bd87494b14637b0359b7
littlebusters <little.busters.reply@gmail.com>
2017-05-10 12:03:41 +0900

    add unzip option

--- Sketch Exchanger.sketchplugin/Contents/Resources/sketch-to-json.sh
+++ Sketch Exchanger.sketchplugin/Contents/Resources/sketch-to-json.sh
@@ -9,7 +9,7 @@ fileName=$1
 fileData="${fileName}.sketch"
 
 if [ -f ${fileData} ]; then
-  unzip ${fileData} -d ${fileName}
+  unzip -o ${fileData} -d ${fileName}
   cd ${fileName}
   find . -name '*.json' -exec sh -c 'cat {} | /usr/local/bin/jq -rS . > {}.1 && mv {}.1 {}' \;
   rm -rf ${fileName}/__MACOSX ${fileName}/.DS_Store
